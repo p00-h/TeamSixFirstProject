372e36fff410da996136c232f76d669eb2aea36a
ParkChanYeong <sidsru21131210@gmail.com>
2025-09-04 20:28:31 +0900

    Reapply "ㅁㄴㅇ"
    This reverts commit bd2ca0e64fd6092499a447fd097f69aca2e7f0a3.

--- TeamSixFirstProject/Character.h
+++ TeamSixFirstProject/Character.h
@@ -13,6 +13,8 @@ private:
     int Defense;
     int Exp;
     int Gold;
+
+    //배틀에서 사용하지 않을 예정입니다 필요없다면 배틀 이외에 사용처가 없다면 제거 요망합니다
     const int ExpToLevelUp = 100;
 
     std::map<std::string, int> Inventory; // 아이템 이름 -> 개수
@@ -27,7 +29,18 @@ public:
     void AddItem(const std::string& item, int count = 1);
     void RemoveItem(const std::string& item, int count = 1);
     void ShowInventory() const;
-
+    //기준점
+    int GetHP() const;
+    void SetHP(const int& hp);
+    int GetAttack() const;
+    void SetAttack(const int& attack);
+    int GetGold() const;
+    void SetGold(const int& gold);
+    int GetExp() const;
+    void SetExp(const int& exp);
+    int GetLevel() const;
+    void SetLevel(const int& level);
+    //기준점 부터 여기까지 (박찬영) 제가 임의로 만든 멤버 함수입니다 
 private:
     void LevelUp();
 };
