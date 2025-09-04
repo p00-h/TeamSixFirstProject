493c02bcf2b4d2c7aab5f92f7aaf6cafddb01831
Bzart148 <dbsrlqowl@gmail.com>
2025-09-04 16:18:30 +0900

    feat 코볼트 일반 공격 추가

--- TeamSixFirstProject/Kobold.cpp
+++ TeamSixFirstProject/Kobold.cpp
@@ -1,4 +1,6 @@
 #include "Kobold.h"
+#include <iostream>
+using namespace std;
 
 Kobold::Kobold(int level)
 	:Monster("Kobold", level)
@@ -8,6 +10,13 @@ Kobold::Kobold(int level)
 														//Monster.h의  HP와 Attack을 protected 사용 시
 	//HP += 10;
 	//Attack += 15;
-														//Kobold.h의 레벨 보너스 대미지 사용 시, Kobold.h 줄 11정의
+														//Kobold.h의 레벨 보너스 대미지 사용 시, Kobold.h 줄 12정의
 	//AttackBouns = level * 2;
 }
+
+//void Kobold::BaseAttack()
+//{
+//	int KoboldDamage = GetAttack() + 5;										//레벨 보너스 대미지 사용 안할 시
+//	int KoboldDamage = GetAttack() + AttackBouns;				//레벨 보너스 대미지 사용 시 Kobodl.h 줄 12 정의
+//	cout << GetName() << "의 일반 공격, 대미지 : " << KoboldDamage << endl;
+//}
--- TeamSixFirstProject/Kobold.h
+++ TeamSixFirstProject/Kobold.h
@@ -7,6 +7,8 @@ class Kobold : public Monster
 	public:
 		Kobold(int level);
 
+		//void BaseAttack();		//코볼트 일반 공격 정의, Kobold.cpp 줄 17구현
+		
 //private:
 //	int AttackBouns;	//레벨에 따른 보너스 정의
 };
