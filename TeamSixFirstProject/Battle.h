a19b252855f8c50b8bb76f870440941387eaf7e3
ParkChanYeong <sidsru21131210@gmail.com>
2025-09-04 20:37:38 +0900

    Docs 배틀 수정

--- TeamSixFirstProject/Battle.h
+++ TeamSixFirstProject/Battle.h
@@ -1,10 +1,8 @@
 #pragma once
 #include <iostream>
 
-class Player;
-class Monster;
 class Battle
 {
 public:
-	Battle(Player* player, Monster* monster);
+	Battle(Character* Character, Monster* monster);
 };
