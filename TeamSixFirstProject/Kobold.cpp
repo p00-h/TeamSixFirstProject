57f5578ee148efbc1db6457a7ee730f80ce64925
Bzart148 <dbsrlqowl@gmail.com>
2025-09-05 17:50:29 +0900

    feat 몬스터 스킬 3종류씩 추가 Monster.cpp에 AttackBouns 0값 초기화

--- TeamSixFirstProject/Kobold.cpp
+++ TeamSixFirstProject/Kobold.cpp
@@ -11,13 +11,25 @@ Kobold::Kobold(int level)
 														//Monster.h의  HP와 Attack을 protected 사용 시
 	//HP += 10;
 	//Attack += 15;
-														//Kobold.h의 레벨 보너스 대미지 사용 시, Kobold.h 줄 12정의
+														//Kobold.h의 레벨 보너스 대미지 사용 시, Kobold.h 줄 14정의
 	//AttackBouns = level * 2;
 }
 
-//void Kobold::BaseAttack()
-//{
-//	int KoboldDamage = GetAttack() + 5;										//레벨 보너스 대미지 사용 안할 시
-//	int KoboldDamage = GetAttack() + AttackBouns;				//레벨 보너스 대미지 사용 시 Kobodl.h 줄 12 정의
-//	cout << GetName() << "의 일반 공격, 대미지 : " << KoboldDamage << endl;
-//}
+void Kobold::PickaxeAttack()
+{
+	int PickaxeAttackDamage = GetAttack() * 1.2;										//레벨 보너스 대미지 사용 안할 시
+//	int KoboldDamage = GetAttack() + AttackBouns;				//레벨 보너스 대미지 사용 시 Kobodl.h 줄 14 정의
+	cout << GetName() << "의 일반 공격, 대미지 : " << PickaxeAttackDamage << endl;
+}
+
+void Kobold::OreThrow()
+{
+	int OreThrowDamage = GetAttack() * 1.4;
+	cout << GetName() << "의 광물 던진기, 대미지 : " << OreThrowDamage << endl;
+}
+
+void Kobold::TramCarRush()
+{
+	int TramCarRushDamage = GetAttack() * 1.8;
+	cout << GetName() << "의 광차 러쉬! 대미지 : " << TramCarRushDamage << endl;
+}
--- TeamSixFirstProject/Orc.cpp
+++ TeamSixFirstProject/Orc.cpp
@@ -1,5 +1,6 @@
 #include "Orc.h"
 #include <iostream>
+using namespace std;
 
 Orc::Orc(int level)
 	:Monster("Orc", level)
@@ -9,4 +10,22 @@ Orc::Orc(int level)
 	SetMP(0);
 										//레벨 보너스 대미지 사용 시
 	//OrcBouns = level * 5;
+}
+
+void Orc::BellyFatAttack()
+{
+	int BellyFatAttackDamage = GetAttack() * 1.5;
+	cout << GetName() << "의 뱃살 공격! 기분이 나쁘다.. 대미지 : " << BellyFatAttackDamage << endl;
+}
+
+void Orc::ClubAttack()
+{
+	int ClubAttackDamage = GetAttack() * 1.2;
+	cout << GetName() << "의 몽둥이 공격! 대미지 : " << ClubAttackDamage << endl;
+}
+
+void Orc::GroundShaking()
+{
+	int GroundShakingDamage = GetAttack() * 2;
+	cout << GetName() << "의 땅울림!! 대미지 : " << GroundShakingDamage << endl;
 }
