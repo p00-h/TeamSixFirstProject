1ec14722f3f00f7bbe453ce9b985c41ab4bdbf8d
ggyu-ik <ggye0723@gmail.com>
2025-09-08 10:41:13 +0900

    feat 아이템 Turn관련 기능 추가

--- TeamSixFirstProject/Item.h
+++ TeamSixFirstProject/Item.h
@@ -9,15 +9,18 @@ protected:
 	std::string Name;
 	int Price;
 	int Value;
+	int Turn;
 
-	Item(std::string name, int price, int value);
+	Item(std::string name, int price, int value, int turn);
 
 public:
 	std::string GetName() const;
 	int GetPrice() const;
-	
+	void TurnDecrease();
 	virtual std::string ItemInfo() = 0;
+
 	virtual void Use(Character* character) = 0;
+	virtual void ExpireEffect(Character* character);
 	virtual Item* Clone() const = 0;
 
 	virtual ~Item() = default;
