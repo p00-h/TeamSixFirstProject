f095f425391adb5a50aaaf8949a032e98437fcaf
ggyu-ik <ggye0723@gmail.com>
2025-09-10 15:02:23 +0900

    refactor Turn관련 변수, 함수 모두 삭제
    아이템 턴 관련 기능 모두 삭제하고 아이템이 지속형인지, 활성화(사용되는중)인지 판단하는 함수 추가

--- TeamSixFirstProject/AttackBoost.h
+++ TeamSixFirstProject/AttackBoost.h
@@ -7,7 +7,7 @@ class AttackBoost :public Item
 public:
 	AttackBoost();
 	virtual bool Use(Character* character) override;
-	virtual void TurnDecrease(Character* character) override;
 	virtual std::string ItemInfo() const override;
 	virtual Item* Clone() const override;
+	virtual bool IsDurationBased() const override;
 };
--- TeamSixFirstProject/DefenseBoost.h
+++ TeamSixFirstProject/DefenseBoost.h
@@ -7,7 +7,7 @@ class DefenseBoost :public Item
 public:
 	DefenseBoost();
 	virtual bool Use(Character* character) override;
-	virtual void TurnDecrease(Character* character) override;
 	virtual std::string ItemInfo() const override;
 	virtual Item* Clone() const override;
+	virtual bool IsDurationBased() const override;
 };
